5921122a971adbcb8ed17cb626e165beea121ba4
JeongHunHui <deca3248@gmail.com>
2023-11-13 01:08:04 +0900

    feat: ✅ 방문 날짜를 입력 받는다.

--- src/main/java/christmas/controller/PlannerController.java
+++ src/main/java/christmas/controller/PlannerController.java
@@ -1,16 +1,21 @@
 package christmas.controller;
 
+import christmas.dto.request.VisitDateRequest;
+import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class PlannerController {
 
     private final OutputView output;
+    private final InputView input;
 
-    public PlannerController(OutputView output) {
+    public PlannerController(OutputView output, InputView input) {
         this.output = output;
+        this.input = input;
     }
 
     public void run() {
         output.writeVisitDateInputMessage();
+        VisitDateRequest visitDateRequest = input.readVisitDate();
     }
 }
