e15cdd6cf634dc960da764d63a90381ce079a2c0
JeongHunHui <deca3248@gmail.com>
2023-11-12 22:12:44 +0900

    feat: Planner 어플리케이션에서 전반적으로 사용되는 상수를 저장하는 PlannerConstant 클래스 생성

---
+++ src/main/java/christmas/constant/PlannerConstant.java
@@ -0,0 +1,10 @@
+package christmas.constant;
+
+public final class PlannerConstant {
+
+    public static final Integer EVENT_MONTH = 12;
+
+    private PlannerConstant() {
+        // 상수 클래스이므로 인스턴스 생성 방지
+    }
+}
