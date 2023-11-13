5c9259be23385f65c767c5812831a72b5e146486
JeongHunHui <deca3248@gmail.com>
2023-11-14 03:59:07 +0900

    feat: ✅ 주문할 메뉴가 중복되면 예외를 발생시킨다.

--- src/main/java/christmas/dto/request/OrderRequest.java
+++ src/main/java/christmas/dto/request/OrderRequest.java
@@ -2,6 +2,7 @@ package christmas.dto.request;
 
 import christmas.constant.menu.Menu;
 import christmas.constant.menu.MenuBoard;
+import christmas.exception.InvalidOrderException;
 import christmas.model.MenuCount;
 import christmas.model.OrderInfo;
 import java.util.EnumMap;
@@ -17,6 +18,9 @@ public class OrderRequest {
 
         for (OrderInfo orderInfo : orderInfos) {
             Menu menu = MenuBoard.getInstance().getMenu(orderInfo.menuName());
+            if (order.containsKey(menu)) {
+                throw new InvalidOrderException();
+            }
             order.put(menu, orderInfo.menuCount());
         }
     }
