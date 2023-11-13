508b4cba36b737580c60b781f9cf70e978eeaa57
JeongHunHui <deca3248@gmail.com>
2023-11-14 03:56:13 +0900

    feat: ✅ 메뉴의 개수가 1 이상의 정수가 아니면 예외를 발생시킨다.

--- src/main/java/christmas/model/MenuCount.java
+++ src/main/java/christmas/model/MenuCount.java
@@ -1,5 +1,18 @@
 package christmas.model;
 
+import christmas.exception.InvalidOrderException;
+
 public record MenuCount(Integer menuCount) {
-    
+
+    private static final Integer MIN_COUNT = 1;
+
+    public MenuCount {
+        if (isMenuCountOutOfRange(menuCount)) {
+            throw new InvalidOrderException();
+        }
+    }
+
+    private Boolean isMenuCountOutOfRange(Integer menuCount) {
+        return menuCount < MIN_COUNT;
+    }
 }
