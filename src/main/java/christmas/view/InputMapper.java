bfb408b07efff77c347dbdf66a807369f3fdb0b7
JeongHunHui <deca3248@gmail.com>
2023-11-14 03:49:53 +0900

    feat: 입력 받은 문자열을 OrderRequest로 매핑해주는 메서드 구현

--- src/main/java/christmas/view/InputMapper.java
+++ src/main/java/christmas/view/InputMapper.java
@@ -1,15 +1,45 @@
 package christmas.view;
 
+import christmas.dto.request.OrderRequest;
 import christmas.dto.request.VisitDateRequest;
+import christmas.exception.InvalidOrderException;
 import christmas.exception.InvalidVisitDateException;
+import christmas.model.MenuCount;
+import christmas.model.MenuName;
+import christmas.model.OrderInfo;
 import christmas.validator.InputValidator;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputMapper {
+
+    private static final String ORDER_DELIMITER = ",";
+    private static final String MENU_COUNT_DELIMITER = "-";
 
     public VisitDateRequest inputToVisitDateRequest(String input) {
         if (InputValidator.isNonInteger(input)) {
             throw new InvalidVisitDateException();
         }
         return new VisitDateRequest(Integer.valueOf(input));
+    }
+
+    public OrderRequest inputToOrderRequest(String input) {
+        if (InputValidator.isInvalidOrderInput(input)) {
+            throw new InvalidOrderException();
+        }
+
+        return new OrderRequest(inputToOrderInfos(input));
+    }
+
+    private List<OrderInfo> inputToOrderInfos(String input) {
+        return Arrays.stream(input.split(ORDER_DELIMITER))
+            .map(info -> info.split(MENU_COUNT_DELIMITER)).map(this::makeOrderInfo).toList();
+    }
+
+    private OrderInfo makeOrderInfo(String[] orderInfoArray) {
+        return new OrderInfo(
+            new MenuName(orderInfoArray[0]),
+            new MenuCount(Integer.valueOf(orderInfoArray[1]))
+        );
     }
 }
