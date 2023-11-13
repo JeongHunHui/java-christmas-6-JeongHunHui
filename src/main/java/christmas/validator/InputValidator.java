5f98c3a2ca148c3288cda518fa4c907765e5c71e
JeongHunHui <deca3248@gmail.com>
2023-11-14 03:48:38 +0900

    feat: 주문 입력 시 입력 형식에 맞는지 확인하는 메서드 구현

--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -5,6 +5,8 @@ import java.util.regex.Pattern;
 public final class InputValidator {
 
     private static final Pattern NON_INTEGER_PATTERN = Pattern.compile("^(?!-?\\d+$).+");
+    private static final Pattern ORDER_INPUT_PATTERN = Pattern.compile(
+        "^([a-zA-Z가-힣0-9]+-\\d+)(,[a-zA-Z가-힣0-9]+-\\d+)*");
 
     private InputValidator() {
         // 인스턴스 생성 방지
@@ -12,5 +14,9 @@ public final class InputValidator {
 
     public static Boolean isNonInteger(String input) {
         return NON_INTEGER_PATTERN.matcher(input).matches();
+    }
+
+    public static Boolean isInvalidOrderInput(String input) {
+        return !ORDER_INPUT_PATTERN.matcher(input).matches();
     }
 }
