04e0d2d40a886e618cabc987113a95b607fb0c47
JeongHunHui <deca3248@gmail.com>
2023-11-13 22:42:25 +0900

    feat: 메뉴들의 정보를 저장하고 있는 메뉴판의 역할을 하는 MenuBoard 싱글톤 클래스 생성

---
+++ src/main/java/christmas/constant/menu/MenuBoard.java
@@ -0,0 +1,33 @@
+package christmas.constant.menu;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MenuBoard {
+
+    private static MenuBoard instance;
+
+    private final Map<String, Menu> menuMap;
+
+    private MenuBoard(Map<String, Menu> menuMap) {
+        this.menuMap = menuMap;
+    }
+
+    public static MenuBoard getInstance() {
+        if (instance == null) {
+            instance = new MenuBoard(makeMenuMap());
+        }
+        return instance;
+    }
+
+    private static Map<String, Menu> makeMenuMap() {
+        Map<String, Menu> menuMap = new HashMap<>();
+        Arrays.stream(Menu.values()).map(menu -> menuMap.put(menu.getName(), menu));
+        return menuMap;
+    }
+
+    public Boolean isInvalidMenuName(String name) {
+        return !menuMap.containsKey(name);
+    }
+}
