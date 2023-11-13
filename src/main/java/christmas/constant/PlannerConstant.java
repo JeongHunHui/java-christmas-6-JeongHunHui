3aeb5a5a2bb55570ed7474359c14f657e833a110
JeongHunHui <deca3248@gmail.com>
2023-11-13 17:25:48 +0900

    fix: 이벤트 달을 java.time.Month을 이용하도록 수정

--- src/main/java/christmas/constant/PlannerConstant.java
+++ src/main/java/christmas/constant/PlannerConstant.java
@@ -1,8 +1,10 @@
 package christmas.constant;
 
+import java.time.Month;
+
 public final class PlannerConstant {
 
-    public static final Integer EVENT_MONTH = 12;
+    public static final Month EVENT_MONTH = Month.DECEMBER;
 
     private PlannerConstant() {
         // 상수 클래스이므로 인스턴스 생성 방지
--- src/main/java/christmas/constant/PlannerMessage.java
+++ src/main/java/christmas/constant/PlannerMessage.java
@@ -2,13 +2,13 @@ package christmas.constant;
 
 public final class PlannerMessage {
 
-    private static final String VISIT_DATE_INPUT_MESSAGE = "%s월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String VISIT_DATE_INPUT_MESSAGE = "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
     private PlannerMessage() {
         // 상수 클래스이므로 인스턴스 생성 방지
     }
 
     public static String getVisitDateInputMessage() {
-        return String.format(VISIT_DATE_INPUT_MESSAGE, PlannerConstant.EVENT_MONTH);
+        return String.format(VISIT_DATE_INPUT_MESSAGE, PlannerConstant.EVENT_MONTH.getValue());
     }
 }
