8a930dd74b36b3b90da35443f677f2c25cca6b54
JeongHunHui <deca3248@gmail.com>
2023-11-14 06:41:09 +0900

    refactor: 잘못된 MenuName 생성 시 InvalidMenuNameException을 발생시키도록 리팩토링

--- src/main/java/christmas/model/MenuName.java
+++ src/main/java/christmas/model/MenuName.java
@@ -1,13 +1,14 @@
 package christmas.model;
 
+import christmas.constant.DetailErrorMessage;
 import christmas.constant.menu.MenuBoard;
-import christmas.exception.InvalidOrderException;
+import christmas.exception.InvalidMenuNameException;
 
 public record MenuName(String menuName) {
 
     public MenuName {
         if (MenuBoard.getInstance().isInvalidMenuName(menuName)) {
-            throw new InvalidOrderException();
+            throw new InvalidMenuNameException(DetailErrorMessage.MENU_NAME_NOT_IN_MENU_BOARD);
         }
     }
 }
