25960b13c3618931c992f1dee990de83d525a7dd
JeongHunHui <deca3248@gmail.com>
2023-11-15 06:18:51 +0900

    feat: 증정 메뉴 출력 메시지를 만드는 메서드 구현

--- src/main/java/christmas/view/OutputMaker.java
+++ src/main/java/christmas/view/OutputMaker.java
@@ -1,14 +1,17 @@
 package christmas.view;
 
 import christmas.constant.menu.Menu;
+import christmas.model.MenuAndCount;
 import christmas.model.MenuCount;
 import christmas.model.Order;
+import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 
 public class OutputMaker {
 
     private static final String ORDER_FORMAT = "%s %d개";
+    private static final String PRESENT_MENU_FORMAT = "%s %d개";
     private static final String NEW_LINE = "\n";
 
     public String makeOrderOutput(Order order) {
@@ -18,6 +21,15 @@ public class OutputMaker {
             MenuCount menuCount = orderMap.get(menu);
             stringJoiner.add(String.format(ORDER_FORMAT, menu.getName(), menuCount.menuCount()));
         }
+        return stringJoiner.toString();
+    }
+
+    public String makePresentMenusOutput(List<MenuAndCount> presentMenuAndCounts) {
+        StringJoiner stringJoiner = new StringJoiner(NEW_LINE);
+        presentMenuAndCounts.forEach(
+            menuAndCount -> stringJoiner.add(
+                String.format(PRESENT_MENU_FORMAT, menuAndCount.menu().getName(),
+                    menuAndCount.menuCount().menuCount())));
         return stringJoiner.toString();
     }
 }
