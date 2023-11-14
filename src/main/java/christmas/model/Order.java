a923574f94d12eb4d26ba52a1ac224338c9520c1
JeongHunHui <deca3248@gmail.com>
2023-11-14 15:39:00 +0900

    refactor: Order 클래스 유효성 검증 시 유효성 검증 메서드에서 검증 후 예외를 던지도록 리팩토링

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -12,23 +12,24 @@ public record Order(Map<Menu, MenuCount> order) {
     private static final Integer MAX_MENU_COUNT = 20;
 
     public Order {
-        if (isTotalCountAboveMax(order)) {
-            throw new InvalidOrderException(DetailErrorMessage.TOTAL_MENU_COUNT_ABOVE_MAX);
-        }
+        validateOrderNotOnlyIncludeDrink(order);
+        validateOrderTotalCountBelowMax(order);
+    }
 
-        if (isOrderOnlyIncludeDrink(order)) {
+    private void validateOrderNotOnlyIncludeDrink(Map<Menu, MenuCount> order) {
+        final Boolean isOrderOnlyIncludeDrink = order.keySet().stream().allMatch(
+            menu -> menu.getCategory().equals(MenuCategory.DRINK)
+        );
+        if (isOrderOnlyIncludeDrink) {
             throw new InvalidOrderException(DetailErrorMessage.ORDER_ONLY_INCLUDE_DRINK);
         }
     }
 
-    private Boolean isOrderOnlyIncludeDrink(Map<Menu, MenuCount> order) {
-        return order.keySet().stream().allMatch(
-            menu -> menu.getCategory().equals(MenuCategory.DRINK)
-        );
-    }
-
-    private Boolean isTotalCountAboveMax(Map<Menu, MenuCount> order) {
-        return getTotalCount(order) > MAX_MENU_COUNT;
+    private void validateOrderTotalCountBelowMax(Map<Menu, MenuCount> order) {
+        final Boolean isTotalCountAboveMax = getTotalCount(order) > MAX_MENU_COUNT;
+        if (isTotalCountAboveMax) {
+            throw new InvalidOrderException(DetailErrorMessage.TOTAL_MENU_COUNT_ABOVE_MAX);
+        }
     }
 
     private Integer getTotalCount(Map<Menu, MenuCount> order) {
