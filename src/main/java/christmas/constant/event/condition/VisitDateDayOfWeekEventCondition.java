c5927c94fe4388856c0bbc47571ae61a3ecb55d7
JeongHunHui <deca3248@gmail.com>
2023-11-15 04:45:41 +0900

    feat: 방문 날짜와 확인 할 요일의 집합을 통해 방문 날짜가 특정 요일인지 확인하는 EventCondition인 VisitDateDayOfWeekEventCondition클래스 생성

---
+++ src/main/java/christmas/constant/event/condition/VisitDateDayOfWeekEventCondition.java
@@ -0,0 +1,21 @@
+package christmas.constant.event.condition;
+
+import christmas.constant.calendar.EventCalendar;
+import christmas.model.Order;
+import christmas.model.VisitDate;
+import java.time.DayOfWeek;
+import java.util.Set;
+
+public class VisitDateDayOfWeekEventCondition implements EventCondition {
+
+    private final Set<DayOfWeek> dayOfWeekSet;
+
+    public VisitDateDayOfWeekEventCondition(Set<DayOfWeek> dayOfWeekSet) {
+        this.dayOfWeekSet = dayOfWeekSet;
+    }
+
+    @Override
+    public Boolean isEventApplied(Order order, VisitDate visitDate) {
+        return dayOfWeekSet.contains(EventCalendar.getInstance().getEventDayOfWeek(visitDate));
+    }
+}
