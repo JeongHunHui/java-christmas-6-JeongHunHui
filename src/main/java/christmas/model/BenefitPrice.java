2fa13222e085eae17b0d89348d0577d13b3c72ec
JeongHunHui <deca3248@gmail.com>
2023-11-15 19:47:18 +0900

    feat: 혜택금액을 저장하는 값 객체인 BenefitPrice 클래스 생성

---
+++ src/main/java/christmas/model/BenefitPrice.java
@@ -0,0 +1,30 @@
+package christmas.model;
+
+import christmas.constant.DetailErrorMessage;
+import christmas.exception.InvalidPriceException;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public record BenefitPrice(Integer price) {
+
+    private static final Integer MIN_PRICE = 0;
+    private static final Integer MINUS_ONE = -1;
+    private static final String CURRENCY_STRING = "원";
+    private static final NumberFormat formatter = NumberFormat.getInstance(Locale.US);
+
+    public BenefitPrice {
+        validateAboveMinPrice(price);
+    }
+
+    private void validateAboveMinPrice(Integer price) {
+        final Boolean isBelowMinPrice = price < MIN_PRICE;
+        if (isBelowMinPrice) {
+            throw new InvalidPriceException(DetailErrorMessage.PRICE_CAN_NOT_BELOW_MIN_PRICE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return formatter.format(price * MINUS_ONE) + CURRENCY_STRING;
+    }
+}
