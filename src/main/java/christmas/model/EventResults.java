3a7f7af8932de70a0f6f4b0040e73a4210626089
JeongHunHui <deca3248@gmail.com>
2023-11-15 19:49:31 +0900

    feat: EventResult들을 관리하는 일급 컬렉션인 EventResults 클래스 생성

---
+++ src/main/java/christmas/model/EventResults.java
@@ -0,0 +1,42 @@
+package christmas.model;
+
+import java.util.Collections;
+import java.util.List;
+
+public record EventResults(List<EventResult> results) {
+
+    public List<MenuAndCount> getPresentMenuAndCounts() {
+        return results.stream()
+            .filter(EventResult::isPresentMenuExists)
+            .flatMap(eventResult -> eventResult.presentMenuAndCounts().stream())
+            .toList();
+    }
+
+    public BenefitPrice getTotalBenefitPrice() {
+        return new BenefitPrice(
+            results.stream()
+                .map(EventResult::benefitPrice)
+                .mapToInt(BenefitPrice::price)
+                .sum()
+        );
+    }
+
+    public DiscountPrice getTotalDiscountPrice() {
+        return new DiscountPrice(
+            results.stream()
+                .filter(result -> !result.isPresentMenuExists())
+                .map(EventResult::benefitPrice)
+                .mapToInt(BenefitPrice::price)
+                .sum()
+        );
+    }
+
+    public Boolean isEmpty() {
+        return results.isEmpty();
+    }
+
+    @Override
+    public List<EventResult> results() {
+        return Collections.unmodifiableList(results);
+    }
+}
